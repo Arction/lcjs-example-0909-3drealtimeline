f6a03f7b9f69abd7652d3f5c499aa296f316fa5c
snekw <dev.snekw@gmail.com>
2021-01-27 13:28:14 +0200

    LCJS 2.2.0

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# lcjs-example-0909-3dRealtimeLine
+# 3D Realtime Line Chart
+
+![3D Realtime Line Chart](3dRealtimeLine.png)
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+- Open the project folder in a terminal:
+
+        npm install              # fetches dependencies
+        npm start                # builds an application and starts the development server
+
+- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+
+
+## Description
+
+Example showcasing the use of 3D line series in a high performance application, with constantly changing data.
+
+
+## API Links
+
+* [3D chart]
+* [3D line series]
+
+
+## Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [Arction][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+
+[0]: https://github.com/Arction/
+[1]: https://www.arction.com/lightningchart-js-api-documentation/
+[2]: https://www.arction.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://www.arction.com/support-services/
+
+© Arction Ltd 2009-2020. All rights reserved.
+
+
+[3D chart]: https://www.arction.com/lightningchart-js-api-documentation/v2.2.0/classes/chart3d.html
+[3D line series]: https://www.arction.com/lightningchart-js-api-documentation/v2.2.0/classes/lineseries3d.html
 
---
+++ src/index.js
@@ -0,0 +1,177 @@
+/*
+ * LightningChartJS example that showcases LineSeries3D in a realtime application (continuous high speed data input).
+ */
+// Import LightningChartJS
+const lcjs = require('@arction/lcjs')
+
+// Extract required parts from LightningChartJS.
+const {
+    lightningChart,
+    AxisTickStrategies,
+    ColorRGBA,
+    ColorHEX,
+    SolidFill,
+    SolidLine,
+    AxisScrollStrategies,
+    Themes
+} = lcjs
+
+// Extract required parts from xyData.
+const {
+    createProgressiveTraceGenerator
+} = require('@arction/xydata')
+
+// Initiate chart
+const chart3D = lightningChart().Chart3D({
+    // theme: Themes.dark
+})
+    // Set 3D bounding box dimensions to highlight X Axis. 
+    .setBoundingBox({ x: 1.0, y: 0.5, z: 0.4 })
+    .setTitle('3D Realtime Line Series')
+
+// Set Axis titles
+chart3D.getDefaultAxisX().setTitle('Axis X')
+chart3D.getDefaultAxisY().setTitle('Axis Y')
+chart3D.getDefaultAxisZ().setTitle('')
+
+// Disable Z Axis ticks as it doesn't represent any actual data dimension (only visual perspective).
+chart3D.getDefaultAxisZ().setTickStrategy( AxisTickStrategies.Empty )
+
+// Define Series configuration for simplified example modification.
+const seriesConf = [
+    {
+        name: 'Series A',
+        thickness: 4,
+        color: ColorHEX('2E3D7C')
+    },
+    {
+        name: 'Series B',
+        thickness: 4,
+        color: ColorHEX('282528')
+    },
+    {
+        name: 'Series C',
+        thickness: 4,
+        color: ColorHEX('BA292E')
+    },
+    {
+        name: 'Series D',
+        thickness: 4,
+        color: ColorHEX('E15D3A')
+    },
+    {
+        name: 'Series E',
+        thickness: 4,
+        color: ColorHEX('FFA73C')
+    },
+]
+
+// Configure Progressive X Axis.
+chart3D.getDefaultAxisX().setInterval(-1000, 0).setScrollStrategy(AxisScrollStrategies.progressive)
+
+// Set Z Axis interval immediately.
+chart3D.getDefaultAxisZ().setInterval(-1, 1+seriesConf.reduce((prev, cur, i) => Math.max(prev, i), 0), false, true)
+
+// : Create Series and generate test data :
+// Amount of unique data points per Series (looped indefinitely along the X plane)
+const seriesUniqueDataAmount = 2500
+// Amount of new data points pushed each frame to EACH series.
+const pointsPerFrame = 5
+
+Promise.all(
+    seriesConf.map((conf, iSeries) => {
+        const seriesName = conf.name || ''
+        const seriesZ = conf.z || iSeries
+        const seriesThickness = conf.thickness || 5
+        const seriesColor = conf.color || ColorRGBA(255, 255, 255)
+        
+        const series = chart3D.addLineSeries()
+            .setName(seriesName)
+            .setLineStyle(new SolidLine({
+                thickness: seriesThickness,
+                fillStyle: new SolidFill({ color: seriesColor })
+            }))
+    
+        // Generate a static YZ data-set for this series that repeats indefinitely along the X plane.
+        return createProgressiveTraceGenerator()
+            .setNumberOfPoints(seriesUniqueDataAmount / 2)
+            .generate()
+            .toPromise()
+            .then((data) => {
+                // Map XY data to YZ data.
+                return data.map((xy) => ({
+                    y: xy.y,
+                    z: seriesZ
+                }))
+            })
+            .then((data) => {
+                // Repeat data set so that it can be looped indefinitely.
+                return {
+                    series,
+                    data: data.concat(data.slice(1, -1).reverse())
+                }
+            })
+    })
+).then(( seriesAndData ) => {
+    // Setup streaming to all series.
+    let tStart = Date.now()
+    let dataAmount = 0
+    let xPos = 0
+    // Keep track of data currently in each series.
+    seriesAndData.forEach(( item ) => item.currentData = [])
+
+    const pushNewData = () => {
+        for ( let iNewPoint = 0; iNewPoint < pointsPerFrame; iNewPoint ++ ) {
+            for ( const { series, data, currentData } of seriesAndData ) {
+                // Pick YZ coordinates from data set.
+                const yz = data[xPos % data.length]
+                const point = {
+                    x: xPos,
+                    y: yz.y,
+                    z: yz.z
+                }
+                series.add(point)
+                currentData.push(point)
+                dataAmount ++
+            }
+            xPos ++
+        }
+        // Schedule next batch of data.
+        requestAnimationFrame(pushNewData)
+    }
+    pushNewData()
+
+    // Schedule cleaning of old data.
+    const checkCleanupOldData = () => {
+        const minPointsToKeep = 1000
+        for ( let i = 0; i < seriesAndData.length; i ++ ) {
+            const { series, data, currentData } = seriesAndData[i]
+            if (currentData.length < minPointsToKeep)
+                continue
+            const spliceStart = currentData.length - minPointsToKeep
+            const spliceCount = Math.min(minPointsToKeep, currentData.length - spliceStart)
+            const pointsToKeep = currentData.splice(spliceStart, spliceCount)
+            series.clear().add(pointsToKeep)
+            seriesAndData[i].currentData = pointsToKeep
+        }
+    }
+    setInterval(checkCleanupOldData, 1000)
+
+    // Display incoming points amount in Chart title.
+    const title = chart3D.getTitle()
+    let lastReset = Date.now()
+    const updateChartTitle = () => {
+        // Calculate amount of incoming points / second.
+        if (dataAmount > 0 && Date.now() - tStart > 0) {
+            const pps = (1000 * dataAmount) / (Date.now() - tStart)
+            chart3D.setTitle(`${title} (${Math.round(pps)} data points / s)`)
+        }
+        // Reset pps counter every once in a while in case page is frozen, etc.
+        if (Date.now() - lastReset >= 5000) {
+            tStart = lastReset = Date.now()
+            dataAmount = 0
+        }
+    }
+    setInterval(updateChartTitle, 1000)
+})
+
